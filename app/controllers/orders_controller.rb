74f008b6e3b4da3cc3fbe8e5e135626235332224
Kate Shaffer <kate.shaffer@gmail.com>
2016-10-20 16:17:20 -0700

    Corrected logic on the Orders#Edit action.

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -7,8 +7,8 @@ class OrdersController < ApplicationController
   def show; end
 
   def edit
-    @order_id = session[:cart_id]
-    @order = Order.find(@order_id)
+    # @order_id = session[:cart_id]
+    @order = Order.find(params[:id])
     @order_items = @order.order_items
   end
 end
