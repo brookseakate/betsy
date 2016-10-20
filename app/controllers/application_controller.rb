b046e667eaf779a9932d7ec2ef0132f80fa99a30
Kate Shaffer <kate.shaffer@gmail.com>
2016-10-19 20:18:39 -0700

    Built session handling session[:cart_id] logic in application controller.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,15 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_action :set_cart_id
+
+  private
+
+  def set_cart_id
+    if session[:cart_id].nil?
+      new_order = Order.create
+      session[:cart_id] = new_order.id
+    end
+  end
 end
