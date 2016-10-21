5b67cded547193c951424c657def57a8b69819d2
Kate Shaffer <kate.shaffer@gmail.com>
2016-10-20 20:52:43 -0700

    WIP; built some Order aciotns

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -6,8 +6,24 @@ class OrdersController < ApplicationController
 
   def show; end
 
-  def edit
+  def edit # for cart
     @order = Order.find(params[:id])
     @order_items = @order.order_items
+  end
+
+  def update # for cart
+    @order = Order.find(params[:id])
+
+    if @order.update(order_params)
+      redirect_to order_path(@order)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:placed, :email, :mailing_address, :mailing_city, :mailing_state, :mailing_zip, :cc_holder_name, :cc_number, :exp, :cvv, :billing_zip, :status)
   end
 end
