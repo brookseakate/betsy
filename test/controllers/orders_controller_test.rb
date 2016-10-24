f6c2705fac8ef79604a0050aff1fe9cc642d8ab0
Kate Shaffer <kate.shaffer@gmail.com>
2016-10-23 18:13:45 -0700

    Began Orders controller tests. Updated some fixtures to continue coordinating with product fixtures.

--- test/controllers/orders_controller_test.rb
+++ test/controllers/orders_controller_test.rb
@@ -1,10 +1,10 @@
 require 'test_helper'
 
 class OrdersControllerTest < ActionController::TestCase
-  # setup do
-  #   @order = orders(:one)
-  # end
-  #
+  setup do
+    @order = orders(:pending_one)
+  end
+
   # test "should get index" do
   #   get :index
   #   assert_response :success
@@ -23,22 +23,22 @@ class OrdersControllerTest < ActionController::TestCase
   #
   #   assert_redirected_to order_path(assigns(:order))
   # end
-  #
-  # test "should show order" do
-  #   get :show, id: @order
-  #   assert_response :success
-  # end
-  #
-  # test "should get edit" do
-  #   get :edit, id: @order
-  #   assert_response :success
-  # end
-  #
+
+  test "should show order" do
+    get :show, id: @order
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get :edit, id: @order
+    assert_response :success
+  end
+
   # test "should update order" do
   #   patch :update, id: @order, order: {  }
   #   assert_redirected_to order_path(assigns(:order))
   # end
-  #
+
   # test "should destroy order" do
   #   assert_difference('Order.count', -1) do
   #     delete :destroy, id: @order
