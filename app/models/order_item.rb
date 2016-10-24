ad53860f13772d3ab5fa413f233279a8bf32daf6
Kate Shaffer <kate.shaffer@gmail.com>
2016-10-23 17:59:01 -0700

    Built OrderItem model tests & validations

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -2,6 +2,13 @@ class OrderItem < ActiveRecord::Base
   belongs_to :order
   belongs_to :product
 
+  validates :order, presence: true
+  validates :product, presence: true
+  
+  validates :quantity,
+    presence: true,
+    numericality: { only_integer: true, greater_than: 0 }
+
   def subtotal
     self.product.price * self.quantity
   end
--- test/models/order_item_test.rb
+++ test/models/order_item_test.rb
@@ -1,7 +1,52 @@
 require 'test_helper'
 
 class OrderItemTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "Cannot create OrderItem without an Order association" do
+    order_item = OrderItem.new(quantity: 3, product_id: 2)
+
+    assert_not order_item.valid?
+  end
+
+  test "Cannot create OrderItem without a Product association" do
+    order_item = OrderItem.new(quantity: 3, order_id: 2)
+
+    assert_not order_item.valid?
+  end
+
+  test "Cannote create an OrderItem without a quantity that is: integer greater than 0" do
+    # no quantity
+    order_item_one = OrderItem.new(product_id: 1, order_id: 2)
+    assert_not order_item_one.valid?
+
+    # Float quantity
+    order_item_one = OrderItem.new(product_id: 1, order_id: 2, quantity: 3.3)
+    assert_not order_item_one.valid?
+
+    # Quantity 0
+    order_item_one = OrderItem.new(product_id: 1, order_id: 2, quantity: 0)
+    assert_not order_item_one.valid?
+  end
+
+  test "Can create an OrderItem with valid data, it has correct associations" do
+    order_item = OrderItem.new(product_id: 1, order_id: 2, quantity: 3)
+
+    assert order_item.valid?
+
+    order_item.save
+
+    assert_equal order_item.product.id, 1
+    assert_instance_of Product, order_item.product
+
+    assert_equal order_item.order.id, 2
+    assert_instance_of Order, order_item.order
+  end
+
+  test "#subtotal method correctly calculates subtotal" do
+    order_item = order_items(:three)
+    # quantity: 15
+    # product_id: 1
+    # price: 999999
+
+    assert_equal 14999985, order_item.subtotal
+  end
 end
