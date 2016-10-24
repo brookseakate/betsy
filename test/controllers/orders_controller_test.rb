9afeedc901232c3e38749e693164bde545dbfa39
Kate Shaffer <kate.shaffer@gmail.com>
2016-10-23 19:05:40 -0700

    Orders controller tests

--- test/controllers/orders_controller_test.rb
+++ test/controllers/orders_controller_test.rb
@@ -5,45 +5,53 @@ class OrdersControllerTest < ActionController::TestCase
     @order = orders(:pending_one)
   end
 
-  # test "should get index" do
-  #   get :index
-  #   assert_response :success
-  #   assert_not_nil assigns(:orders)
-  # end
-  #
-  # test "should get new" do
-  #   get :new
-  #   assert_response :success
-  # end
-  #
-  # test "should create order" do
-  #   assert_difference('Order.count') do
-  #     post :create, order: {  }
-  #   end
-  #
-  #   assert_redirected_to order_path(assigns(:order))
-  # end
+  test "cart should show correct Order page using session[:cart_id] value" do
+    session[:cart_id] = 1
+
+    get :cart
+    assert_redirected_to edit_order_path(1)
+    assert_equal assigns(:order), Order.find(1)
+  end
 
   test "should show order" do
     get :show, id: @order
     assert_response :success
+    assert_template :show
   end
 
   test "should get edit" do
     get :edit, id: @order
     assert_response :success
+    assert_template :edit
   end
 
-  # test "should update order" do
-  #   patch :update, id: @order, order: {  }
-  #   assert_redirected_to order_path(assigns(:order))
-  # end
+  test "should get checkout" do
+    get :checkout, id: @order
+    assert_response :success
+    assert_template :checkout
+  end
 
-  # test "should destroy order" do
-  #   assert_difference('Order.count', -1) do
-  #     delete :destroy, id: @order
-  #   end
-  #
-  #   assert_redirected_to orders_path
-  # end
+
+  test "should update order" do
+    patch :update, id: @order, order: { email: "a@b.com", cc_number: 11111 }
+
+    assert_response :redirect
+    assert_redirected_to order_path(assigns(:order))
+    assert_equal "a@b.com", assigns(:order).email
+    assert_equal 11111, assigns(:order).cc_number
+  end
+
+  test "update should complete checkout" do
+    patch :update, id: @order, order: { email: "a@b.com", cc_number: 11111 }
+
+    assert_equal "paid", assigns(:order).status
+    assert_not_nil assigns(:order).placed
+  end
+
+  test "update should clear session[:cart_id]" do
+    session[:cart_id] = 55555
+    patch :update, id: @order, order: { email: "a@b.com", cc_number: 11111 }
+
+    assert_nil session[:cart_id]
+  end
 end
