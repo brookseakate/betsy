caccf5ef8318a6167f01d0fb8fe547bd7e39966a
Dianne Laguerta <diannelaguerta@gmail.com>
2016-10-24 12:18:17 -0700

    added helper method for pretty date, applied time zone to application layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def currency(amount)
     sprintf("$%0.02f", amount/100)
   end
+
+  def pretty_date(datetime)
+    datetime.strftime("%I:%M %p, %A, %B %d, %Y (%Z)")
+  end
 end
