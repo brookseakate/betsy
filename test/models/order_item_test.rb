a914bec02d32b36d2429fde52820bd5e8cfb9e84
Kate Shaffer <kate.shaffer@gmail.com>
2016-10-24 22:45:58 -0700

    Added Order_Item instance method tests

--- test/models/order_item_test.rb
+++ test/models/order_item_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class OrderItemTest < ActiveSupport::TestCase
   test "Cannot create OrderItem without an Order association" do
-    order_item = OrderItem.new(quantity: 3, product_id: 2)
+    order_item = OrderItem.new(quantity: 3, product_id: 22)
 
     assert_not order_item.valid?
   end
@@ -10,7 +10,7 @@ class OrderItemTest < ActiveSupport::TestCase
   test "Cannot create OrderItem without a Product association" do
     order_item = OrderItem.new(quantity: 3, order_id: 2)
 
-    assert_not order_item.valid?
+    assert_raise(NoMethodError) { order_item.valid? } # when no product is present, #must_have_enough_stock method cannot call product.inventory; fails with NoMethodError
   end
 
   test "Cannote create an OrderItem without a quantity that is: integer greater than 0" do
@@ -48,5 +48,26 @@ class OrderItemTest < ActiveSupport::TestCase
     # price: 999999
 
     assert_equal 14999985, order_item.subtotal
+  end
+
+  test "#sufficient_stock? method should return false for inventory < quantity" do
+    order_item = order_items(:three)
+
+    assert_equal false, order_item.sufficient_stock?
+  end
+
+  test "#sufficient_stock? method should return true for inventory = quantity" do
+    order_item = order_items(:three)
+    order_item.update(quantity: 10)
+
+    assert_equal true, order_item.sufficient_stock?
+  end
+
+  test "#must_have_enough_stock method should add to errors hash if insufficient_stock" do
+    order_item = order_items(:three)
+
+    order_item.must_have_enough_stock
+
+    assert_includes order_item.errors, :stock
   end
 end
