6e1366aa4d23af045b21de81357fc5cbf3a238d9
Esther Ng <esther.hc.ng@gmail.com>
2016-10-25 11:34:55 -0700

    Resolved test errors in user_test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -5,10 +5,9 @@ class UserTest < ActiveSupport::TestCase
   test "should be able to retrieve all orders associated with user" do
     lil = users(:lil).id
     user = User.find(lil)
-    product = Product.create(user_id: lil, name: "test product", price: 999)
-    item = OrderItem.create(order_id: 2,
-    product_id: product.id, quantity: 2)
-    order = Order.find(2)
+    product = Product.find(99)
+    item = OrderItem.find_by(order_id: 88)
+    order = Order.find(88)
     assert_includes user.orders, order
   end
 
