08fefa49780433d4169cf7a2119c7bfeb33788dc
Kate Shaffer <kate.shaffer@gmail.com>
2016-10-27 13:23:20 -0700

    WIP; refactoring product average rating DRYer

--- app/models/product.rb
+++ app/models/product.rb
@@ -19,4 +19,24 @@ class Product < ActiveRecord::Base
   def in_stock?
     return self.inventory > 0
   end
+
+  def average_rating
+    all_products = Product.all
+    products_and_ratings = []
+
+    # lines 86-98 (everything before the render) are collecting products ordered by the average rating in their reviews (descending) -ks
+    @all_products.each do |product|
+      reviews = product.reviews
+      if !reviews.blank?
+        ratings = reviews.map { |review| !review.rating.blank? ? review.rating : 0 }
+        average_rating = ratings.reduce(:+)/ratings.length
+      else
+        average_rating = 0
+      end
+
+      @products_and_ratings << [product, average_rating]
+    end
+
+    @products = @products_and_ratings.sort_by { |arr| arr[1] }.map { |arr| arr[0] }.reverse
+  end
 end
