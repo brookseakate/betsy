ef9cef9984b44809c41064b575234e3ea36ce9ff
Natasha Ewing <natasharai@me.com>
2016-10-28 01:36:11 -0700

    fixed .git file issue for merge conflict

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,8 +3,19 @@ module ApplicationHelper
     sprintf("$%0.02f", amount/100.to_f)
   end
 
-  def pretty_date(datetime)
-    datetime.strftime("%I:%M %p, %A, %B %d, %Y (%Z)")
+  def copyright_notice_year_range(start_year)
+  # In case the input was not a number (nil.to_i will return a 0)
+  start_year = start_year.to_i
+
+  current_year = Time.new.year
+
+    if current_year > start_year && start_year > 2000
+      "#{start_year} - #{current_year}"
+    elsif start_year > 2000
+      "#{start_year}"
+    else
+      "#{current_year}"
+    end
   end
 
   def user_name
